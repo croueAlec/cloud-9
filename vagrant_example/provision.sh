6aa0f74d9adacce8239c5f1a8ff443a5eba479dd
croueAlec <alec.croue.pro@gmail.com>
2026-03-17 19:25:43 +0100

    fix (vagrant provision) : fix python version mismatch

--- vagrant_example/provision.sh
+++ vagrant_example/provision.sh
@@ -8,15 +8,24 @@ sudo	cat /vagrant/vagrant_example/ssh_key.pub >> /home/vagrant/.ssh/authorized_k
 sudo	chmod 600 /home/vagrant/.ssh/authorized_keys && \
 sudo	chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 
-echo "This is a simulated remote node." >> /home/deploy/README && \
-echo " As per the cloud-1 subject, this node has ubuntu 20.04 LTS, a running SSH daemon and Python installed by default." >> /home/deploy/README
 
 useradd	-m -s /bin/bash deploy
 echo	"deploy:deploy" | chpasswd
 echo	"deploy ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
+echo "This is a simulated remote node." >> /home/deploy/README && \
+echo " As per the cloud-1 subject, this node has ubuntu 20.04 LTS, a running SSH daemon and Python installed by default." >> /home/deploy/README
+
 mkdir -p /home/deploy/.ssh && \
 cat /vagrant/vagrant_example/ssh_key.pub >> /home/deploy/.ssh/authorized_keys && \
 chmod 700 /home/deploy/.ssh/ && \
 chmod 600 /home/deploy/.ssh/authorized_keys && \
-chown deploy:deploy /home/deploy/.ssh/
+chown -R deploy:deploy /home/deploy/
+
+sudo apt-get update && \
+sudo apt-get install -y software-properties-common && \
+sudo add-apt-repository ppa:deadsnakes/ppa && \
+sudo apt-get install -y python3.9 && \
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1 && \
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2 && \
+sudo update-alternatives --config python3
